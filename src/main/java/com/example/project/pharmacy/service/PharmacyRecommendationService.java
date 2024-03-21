99db60adf2e0016cd7fd432ce81f8290c1220a5a
Jung-ji-hyeon <ejxldortm@naver.com>
2024-03-21 15:54:47 +0900

    MockMvc를 이용한 테스트 코드 작성

--- src/main/java/com/example/project/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/project/pharmacy/service/PharmacyRecommendationService.java
@@ -49,7 +49,7 @@ public class PharmacyRecommendationService {
 
     private OutputDto convertToOutputDto(Direction direction) {
         return OutputDto.builder()
-                .pharmacyAddress(direction.getTargetPharmacyName())
+                .pharmacyName(direction.getTargetPharmacyName())
                 .pharmacyAddress(direction.getTargetAddress())
                 .directionUrl("todo") // todo
                 .roadViewUrl("todo")
